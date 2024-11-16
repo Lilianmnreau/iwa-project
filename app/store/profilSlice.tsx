ab12a3b38c5548bb752167a482e59e7aac3b921b
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-11-16 15:06:13 +0100

    profil appel api

--- app/store/profilSlice.tsx
+++ app/store/profilSlice.tsx
@@ -1,30 +1,237 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../models/user.model';
+import { Emplacement } from '../models/emplacement_model';
 
 interface ProfilState {
-    profil_notifications: number;
-    isLoggedIn: boolean; // Ajout de la variable isLoggedIn
+  profil_notifications: number;
+  isLoggedIn: boolean;
+  loading: boolean;
+  error: string | null;
+  users: User[];
 }
 
 const initialState: ProfilState = {
-    profil_notifications: 78,
-    isLoggedIn: true, // État initial pour isLoggedIn
+  profil_notifications: 78,
+  isLoggedIn: true,
+  loading: false,
+  error: null,
+  users: [],
 };
 
 const profilSlice = createSlice({
-    name: 'profil',
-    initialState,
-    reducers: {
-        setProfilNotifications(state, action: PayloadAction<number>) {
-            state.profil_notifications = action.payload;
-        },
-        login(state) { // Action pour la connexion
-            state.isLoggedIn = true;
-        },
-        logout(state) { // Action pour la déconnexion
-            state.isLoggedIn = false;
-        },
+  name: 'profil',
+  initialState,
+  reducers: {
+    fetchProfilStart(state) {
+      state.loading = true;
+      state.error = null;
     },
+    fetchProfilSuccess(state, action: PayloadAction<{ profil_notifications: number; isLoggedIn: boolean }>) {
+      state.profil_notifications = action.payload.profil_notifications;
+      state.isLoggedIn = action.payload.isLoggedIn;
+      state.loading = false;
+    },
+    fetchProfilFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    setProfilNotifications(state, action: PayloadAction<number>) {
+      state.profil_notifications = action.payload;
+    },
+    loginStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    loginSuccess(state) {
+      state.isLoggedIn = true;
+      state.loading = false;
+    },
+    loginFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logoutStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    logoutSuccess(state) {
+      state.isLoggedIn = false;
+      state.loading = false;
+    },
+    logoutFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    fetchUsersStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchUsersSuccess(state, action: PayloadAction<User[]>) {
+      state.users = action.payload;
+      state.loading = false;
+    },
+    fetchUsersFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    addUserStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    addUserSuccess(state, action: PayloadAction<User>) {
+      state.users.push(action.payload);
+      state.loading = false;
+    },
+    addUserFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateUserStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    updateUserSuccess(state, action: PayloadAction<{ id: string; updatedUser: User }>) {
+      const index = state.users.findIndex(user => user.id_user === action.payload.id);
+      if (index !== -1) {
+        state.users[index] = action.payload.updatedUser;
+      }
+      state.loading = false;
+    },
+    updateUserFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess(state, action: PayloadAction<string>) {
+      state.users = state.users.filter(user => user.id_user !== action.payload);
+      state.loading = false;
+    },
+    deleteUserFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    addEmplacementStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    addEmplacementSuccess(state, action: PayloadAction<{ id_user: string; emplacement: Emplacement }>) {
+      const user = state.users.find(user => user.id_user === action.payload.id_user);
+      if (user) {
+        user.emplacements.push(action.payload.emplacement);
+      }
+      state.loading = false;
+    },
+    addEmplacementFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateEmplacementStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    updateEmplacementSuccess(state, action: PayloadAction<{ id_user: string; id_emplacement: string; updatedEmplacement: Emplacement }>) {
+      const user = state.users.find(user => user.id_user === action.payload.id_user);
+      if (user) {
+        const index = user.emplacements.findIndex(emplacement => emplacement.id_emplacement === action.payload.id_emplacement);
+        if (index !== -1) {
+          user.emplacements[index] = action.payload.updatedEmplacement;
+        }
+      }
+      state.loading = false;
+    },
+    updateEmplacementFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteEmplacementStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteEmplacementSuccess(state, action: PayloadAction<{ id_user: string; id_emplacement: string }>) {
+      const user = state.users.find(user => user.id_user === action.payload.id_user);
+      if (user) {
+        user.emplacements = user.emplacements.filter(emplacement => emplacement.id_emplacement !== action.payload.id_emplacement);
+      }
+      state.loading = false;
+    },
+    deleteEmplacementFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    addFavoriteEmplacementStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    addFavoriteEmplacementSuccess(state, action: PayloadAction<{ id_user: string; emplacement: Emplacement }>) {
+      const user = state.users.find(user => user.id_user === action.payload.id_user);
+      if (user) {
+        user.emplacementsFavoris.push(action.payload.emplacement);
+      }
+      state.loading = false;
+    },
+    addFavoriteEmplacementFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    removeFavoriteEmplacementStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    removeFavoriteEmplacementSuccess(state, action: PayloadAction<{ id_user: string; id_emplacement: string }>) {
+      const user = state.users.find(user => user.id_user === action.payload.id_user);
+      if (user) {
+        user.emplacementsFavoris = user.emplacementsFavoris.filter(emplacement => emplacement.id_emplacement !== action.payload.id_emplacement);
+      }
+      state.loading = false;
+    },
+    removeFavoriteEmplacementFailure(state, action: PayloadAction<string>) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
 });
 
-export const { setProfilNotifications, login, logout } = profilSlice.actions; // Exporter les actions
+export const {
+  fetchProfilStart,
+  fetchProfilSuccess,
+  fetchProfilFailure,
+  setProfilNotifications,
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  logoutStart,
+  logoutSuccess,
+  logoutFailure,
+  fetchUsersStart,
+  fetchUsersSuccess,
+  fetchUsersFailure,
+  addUserStart,
+  addUserSuccess,
+  addUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  addEmplacementStart,
+  addEmplacementSuccess,
+  addEmplacementFailure,
+  updateEmplacementStart,
+  updateEmplacementSuccess,
+  updateEmplacementFailure,
+  deleteEmplacementStart,
+  deleteEmplacementSuccess,
+  deleteEmplacementFailure,
+  addFavoriteEmplacementStart,
+  addFavoriteEmplacementSuccess,
+  addFavoriteEmplacementFailure,
+  removeFavoriteEmplacementStart,
+  removeFavoriteEmplacementSuccess,
+  removeFavoriteEmplacementFailure,
+} = profilSlice.actions;
+
 export default profilSlice.reducer;
