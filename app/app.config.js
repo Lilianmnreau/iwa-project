b6a3797ba6f2d3a7ddbbc726c6b49550f4f64c6b
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2024-11-24 18:22:22 +0100

    ci update

--- app/app.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ config }) => ({
-    name: config.name,
-    slug: "app",
-    extra: {
-      eas: {
-        projectId: "2f929093-524e-4740-a38b-bd2df9ad1a18"
-      }
-    },
-  });
