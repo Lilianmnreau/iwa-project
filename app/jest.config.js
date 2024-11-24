ff88f73352142ed2f20273cc7c5f638cd18da348
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-11-24 22:48:18 +0100

    test translation

--- app/jest.config.js
+++ app/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'node_modules/(?!(react-native|@react-native|@react-native-community|expo-constants|expo-font|expo-asset|expo-modules-core|expo|@expo|react-native-gesture-handler|react-redux|@expo/vector-icons|expo-status-bar|react-native-calendars|react-native-swipe-gestures|react-native-maps|@kolking/react-native-rating)/)',
   ],
   transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest', // Transforme les fichiers JS/TS avec Babel
+    '^.+\\.[jt]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
