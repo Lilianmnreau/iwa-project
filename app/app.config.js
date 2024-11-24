63f2b51e1741132f2e09023e068474406eb045da
Lilian Monnereau <128914040+Lilianmnreau@users.noreply.github.com>
2024-11-24 18:16:03 +0100

    Create app.config.js

---
+++ app/app.config.js
@@ -0,0 +1,9 @@
+module.exports = ({ config }) => ({
+    name: config.name,
+    slug: "app",
+    extra: {
+      eas: {
+        projectId: "2f929093-524e-4740-a38b-bd2df9ad1a18"
+      }
+    },
+  });
